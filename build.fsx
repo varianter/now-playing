254f07b5c620632ed81dbaea1f4b03ddc60bf65c
Christian Brevik <christian.brevik@gmail.com>
2019-05-28 22:16:24 +0200

    Add build and provision FAKE-scripts

---
+++ build.fsx
@@ -0,0 +1,59 @@
+// Information specific for this solution
+let appName = "NowPlaying"
+let sln = sprintf "%s.sln" appName
+
+// Dependencies and FAKE-specific stuff
+// ==============================================
+#r "paket:
+nuget Fake.Core.Target
+nuget Fake.DotNet.Cli
+nuget Fake.IO.FileSystem
+nuget Fake.IO.Zip //"
+// Use this for IDE support. Not required by FAKE 5.
+#load ".fake/build.fsx/intellisense.fsx"
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+// ==============================================
+
+// We fetch the version from the current buildServer if possible (TeamCity, VSTS etc.)
+let version = 
+        match BuildServer.buildServer with 
+        | TeamFoundation -> BuildServer.buildVersion
+        | _ -> "1.0.0-beta4"
+
+// Ensure that the 'build' and 'artifacts' directory exists
+let buildDir = __SOURCE_DIRECTORY__ + "/build"
+Directory.ensure buildDir
+let artifactDir = __SOURCE_DIRECTORY__ + "/artifacts"
+Directory.ensure artifactDir
+
+// Clean the 'build' and 'artifacts' folder to prepare for a new build
+Target.create "Clean" <| fun _ ->
+    Shell.cleanDirs [buildDir; artifactDir;]
+
+// Use the given dotnet SDK to restore packages and build the solution into the 'build' folder
+Target.create "Build" <| fun _ ->
+    DotNet.publish (fun opt -> { opt with OutputPath = Some <| sprintf "%s/app" buildDir }) sln
+//Runtime = Some "win-x64" ;
+
+// Zip the 'build' folder and place the zip in the 'artifacts' folder together with the
+// ARM-templates and the upload scripts.
+Target.create "Artifact" <| fun _ ->
+    let artifactFilename = sprintf "%s/%s.%s.zip" artifactDir appName version
+
+    !! "./build/**/*.*" |> Zip.zip buildDir artifactFilename
+
+    let artifactDirArm = (artifactDir + "/infrastructure/")
+    Directory.ensure artifactDirArm
+    Shell.copyDir artifactDirArm "infrastructure" (fun f -> not <| f.EndsWith(".azcli"))
+    Shell.copyFile artifactDir "provision.fsx"
+    Shell.copyFile artifactDir "provision.fsx.lock"
+
+"Clean"
+    ==> "Build"
+    ==> "Artifact"
+
+Target.runOrDefault "Artifact"
