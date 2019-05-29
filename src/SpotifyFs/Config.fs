a41cd54525a388e11eeed47929cdadcfece26c7f
Mikael Brevik <mikaelbre@gmail.com>
2019-05-29 08:34:18 +0200

    Adds initial port of Spotify API to F# (#9)
    * Adds initial port of Spotify API to F#
    
    * Replaces the current.json providers with spotify example
    
    * Makes getAsync functions point free
    
    * Makes static config variables literals
    
    * Removes outcommented record models
    
    * Makes private function naming consistant

---
+++ src/SpotifyFs/Config.fs
@@ -0,0 +1,20 @@
+module Config
+
+open System
+open System.Text
+
+[<Literal>]
+let AccountBaseUrl = "https://accounts.spotify.com"
+
+[<Literal>]
+let ApiBaseUrl ="https://api.spotify.com"
+
+[<Literal>]
+let RequestedScopes = "user-read-currently-playing user-read-recently-played"
+
+let clientId = Environment.GetEnvironmentVariable("SpotifyApiClientId")
+let clientSecret = Environment.GetEnvironmentVariable("SpotifyApiClientSecret")
+let spotifyApiBasicAuthHeaderValue =
+    (clientId + ":" + clientSecret)
+    |> Encoding.Default.GetBytes
+    |> Convert.ToBase64String
---
+++ src/SpotifyFs/SpotifyApi.fs
@@ -0,0 +1,44 @@
+module SpotifyFs
+open FSharp.Data
+open FSharp.Data.HttpRequestHeaders
+
+type RecentlyPlayed = JsonProvider<"./data/recently.json">
+type CurrentTrack = JsonProvider<"./data/current.json">
+type Token = JsonProvider<"./data/token.json">
+type Me = JsonProvider<"./data/me.json">
+
+let private apiUrl = sprintf "%s/me%s" Config.ApiBaseUrl 
+let private map f op = async {
+    let! x = op
+    return f x
+  }
+
+let private getAsync parse path accessToken =
+  Http.AsyncRequestString
+    ( apiUrl path,
+      headers = [ Authorization ("Bearer " + accessToken) ] )
+    |> map parse
+
+let private postTokenRequestAsync body =
+    Http.AsyncRequestString
+      ( sprintf "%s/api/token" Config.AccountBaseUrl,
+        body = FormValues body,
+        headers = [ Authorization ("Basic " + Config.spotifyApiBasicAuthHeaderValue) ] )
+      |> map Token.Parse
+
+let constructAuthorizedRedirectUrl redirectUri state =
+  sprintf "%s/authorize?client_id=%s&response_type=code&redirect_uri=%s&state=%s&scope=%s"
+    Config.AccountBaseUrl Config.clientId redirectUri state Config.RequestedScopes
+
+let getUserInfoAsync = getAsync Me.Parse "/me"
+let getCurrentTrackAsync = getAsync CurrentTrack.Parse "/me/player/currently-playing"
+let getRecentlyPlayedTracksAsync = getAsync RecentlyPlayed.Parse "/me/player/recently-played?limit=50"
+
+let finishAuthorizeAsync code redirectUrl =
+  postTokenRequestAsync [ ("grant_type", "authorization_code"); 
+                     ("code", code); 
+                     ("redirect_uri", redirectUrl); ]
+
+let refreshTokenAsync refreshToken =
+  postTokenRequestAsync [ ("grant_type", "refresh_token"); 
+                     ("refresh_token", refreshToken); ]
